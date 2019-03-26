d1fb790f18acabeebace5387b139f4032d686e26
Guest User <Guest@epicodus-79.local>
2019-03-26 10:23:17 -0700

    inital push, jQuery ajax api

---
+++ weather-interface.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+  $('#weatherLocation').click(function() {
+    let city = $('#location').val();
+    $('#location').val("");
+    $.ajax({
+      url: `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=d3ab595c58d2726f21b19192e060b769`,
+      type: 'GET',
+      data: {
+        format: 'json'
+      },
+      success: function(response) {
+        $('.showHumidity').text(`The humidity in ${city} is ${response.main.humidity}%`);
+        $('.showTemp').text(`The temperature in Kelvins is ${response.main.temp}.`);
+      },
+      error: function() {
+        $('#errors').text("There was an error processing your request. Please try again.");
+      }
+    });
+  });
+});
